2a96b285674b02fbe68a4202c0c132fed482962a
Flowmo <morf2515@usherbrooke.ca>
2026-01-21 12:41:03 -0500

    pio settings updated

--- Firmware_esp32/src/main.cpp
+++ Firmware_esp32/src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 
+AccelStepper MotorX()
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600)
+  
 }
 
 void loop() {
