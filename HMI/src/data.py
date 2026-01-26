2c744fe951b6b42b4aab2007ee59467cf24ebbc3
GabrielD98 <gaby.roberge@homtail.com>
2026-01-26 10:10:13 -0500

    Logic for the storage added

--- HMI/src/data.py
+++ HMI/src/data.py
@@ -3,13 +3,19 @@ from enum import Enum
 
 
 class Type(Enum):
-    RESISTOR = 0
-    DEL = 1
+    RESISTOR = 0    
+    LED = 1
+
+
+class StorageState(Enum):
+    USING = 0   #Can be used by the slicer.
+    IGNORE = 1  #Tells the slicer to ignore these components.
+    EMPTY = 2   #The storage of these components is empty.
 
 
 @dataclass
 class Position:
-    x : float
+    x : float       
     y : float
     z : float 
     rotation : float
@@ -17,8 +23,17 @@ class Position:
 
 @dataclass
 class Piece:
-    position : Position
-    package : str
-    type : Type
-    value : str
+    position : Position     #The position of the piece on the PCB, ignores the offset.
+    package : str           #The string package read by the .pos file of Kicad.
+    type : Type             #The type of the component (ex : resistor, LED).
+    value : str             #The value of the component  (ex : 100 Ohm for a resistor)
+
+
+@dataclass
+class StorageUnit:
+    piece : Piece           #The piece of this storage unit.
+    deltaPos : Position     #The offset between all pieces on this unit.
+    state : StorageState    #The state of the unit, tells the HMI if this unit is available.
+    quantity : int          #The amount of components stored.
+    automatic : bool        #Is the feeder automatic or not.
 
---
+++ HMI/src/storage.py
@@ -0,0 +1,87 @@
+from typing import List
+from data import StorageUnit, Piece, Position, StorageState
+
+class Storage :   
+    """
+    Represents the logic for the storage of the components to place
+    This class is a singleton
+
+    Attributes:
+        components (dict[str, StorageUnit]): 
+            a list a all the components that the storage handles. they will be added by the user in the HMI.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Storage. If an object already has been created, it does nothing.
+        """
+        if hasattr(self, 'initialized'):
+            return
+        
+        self.components:dict[str, StorageUnit] = {}
+        self.initialized = True
+
+
+
+
+    def __new__(cls):
+        """
+        If a 'Storage' object already has been created, returns the one created before.
+
+        Else, it creates a new 'Storage' object and returns it.
+        """
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Storage, cls).__new__(cls)
+        return cls.instance
+
+
+
+
+    def addComponent(self, piece:Piece, deltaPos:Position, state:StorageState, quantity:int):
+        """
+        Adds a component to the storage. This allows the slicer to know wich components will be
+        available to place. 
+
+        Parameters:
+            piece (Piece): The component's data to be added to this storage unit.
+            deltaPos (Position): The offset of each pieces in this storage unit. 
+            state (StorageState): The state of this unit -> USING, IGNORE, EMPTY.
+            quantity (int): The amount of available components in this unit. 
+        """
+        if piece.package in self.components:
+            return
+        component = StorageUnit(piece=piece, deltaPos=deltaPos, state=state, quantity=quantity)
+        self.components[piece.package] = component
+
+
+
+
+    def removeComponent(self, piece:Piece):
+        """
+        Removes a component that has been added before by the user. If the component
+        is not found, does nothing
+
+        Parameters:
+            piece (Piece): The component's data to be removed to this storage unit.
+        """
+        if not piece.package in self.components:
+            return
+        del self.components[piece.package]
+
+
+
+
+    def getValidComponents(self) -> List[Piece]:
+        """
+        Returns a list of all the components that can be treated by the slicer. 
+        To be valid, the state has to be set to 'USING".
+
+        Returns:
+            (List[Piece]): The list of all the valid pieces.
+        """
+        valid_components:List[Piece] = []
+        for component in self.components.values():
+            if component.state == StorageState.USING:
+                valid_components.append(component)
+        return valid_components
+        
