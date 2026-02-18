50ba6ad7b97e3ac469528a98ab3f951a0a9166ef
EloiCharb <eloi401216@gmail.com>
2026-02-18 13:33:54 -0500

    communication task on core 1

--- Firmware_esp32/src/main.cpp
+++ Firmware_esp32/src/main.cpp
@@ -1,17 +1,34 @@
 #include "Arduino.h"
 #include "communication.hpp"
 
+#define ENABLE_TEST true
+
+
 void setup()
 {
-  Serial.begin(115200);
-  delay(1000); // Wait for serial to initialize
-  
-  waitForConnection(); // Wait for Python to connect
+	Serial.begin(115200);
+	delay(1000); // Wait for serial to initialize
+
+	xTaskCreatePinnedToCore(
+		communicationLoop,
+		"communicationTask",
+		512,
+		NULL, // add controller address here
+		1,
+		NULL,
+		0
+	);
+
+	if(ENABLE_TEST)
+	{
+
+	}
+
+
 }
 
 
 void loop()
 {
-  communicationLoop();
 }
 
