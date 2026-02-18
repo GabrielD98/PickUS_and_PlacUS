900dc50252244a267db1ee9fcb7f6725bc634328
daphparis <duffyduck9704@gmail.com>
2026-02-18 13:32:34 -0500

    Fonction moteur
    Premiere fonction de test

--- Firmware_esp32/src/main.cpp
+++ Firmware_esp32/src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
-// HELLO I AM HERE TOO
-// put function declarations here:
-int myFunction(int, int);
+#include "MotorControl.h"
+ 
+AccelStepper motor(1,4,5);  //1=using a driver, 11=step, 12=dir
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600)
+void setup()
+{
+  Serial.begin(115200);
+
+  //Ligne pour tester enable manuellement (A retravailler)
+  //pinMode(46, OUTPUT);
+  //digitalWrite(46, HIGH);
+
+  //motor.move(400); //relative
+  //motor.moveTo(400); //absolute
   
+  MoveToPos(motor, 400, 800, 400);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+
+  //MoveToPosition(motor,1500,500);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
