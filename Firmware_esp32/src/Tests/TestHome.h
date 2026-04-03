7129aabe0b2f5a0decc6cb247f61634a9a839378
Flowmo <morf2515@usherbrooke.ca>
2026-04-03 15:03:11 -0400

    Tests commentaries wip

--- Firmware_esp32/src/Tests/TestHome.h
+++ Firmware_esp32/src/Tests/TestHome.h
@@ -3,12 +3,29 @@
 
 #include "../Controller.h"
 
+/**
+ * @brief Confirms the machine moves towards the origin corner until it reaches a limit switch,
+ * one degree of freedom at a time.
+ */
 class TestHome
 {
 public:
+    /**
+     * @brief Instantiate a local controller, independant from the UI.
+     */
     TestHome();
+
+    /**
+     * @brief Targets a test position to go to, then commands to home.
+     * Updates the machine state at a certain loop count.
+     * 
+     * @return true if passed, else false.
+     */
     bool run();
 private:
+    /**
+     * @brief Pointer to local controller.
+     */
     Controller* testCtrl;
 };
 
